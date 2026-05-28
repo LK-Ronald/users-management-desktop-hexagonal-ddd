150f844604bb41a004e14e1bd659f2c6c82e7b4d
LkRonald <rpereirac@unicartagena.co>
2026-05-28 09:27:26 -0500

    feat(mapper): add ProducerApplicationMapper for mapping between commands, queries, and domain models

---
+++ src/main/java/com/jcaa/usersmanagement/application/service/mapper/ProducerApplicationMapper.java
@@ -0,0 +1,30 @@
+package com.jcaa.usersmanagement.application.service.mapper;
+
+import com.jcaa.usersmanagement.application.service.dto.command.CreateProducerCommand;
+import com.jcaa.usersmanagement.application.service.dto.command.DeleteUserCommand;
+import com.jcaa.usersmanagement.domain.enums.ProducerEntityType;
+import com.jcaa.usersmanagement.domain.enums.ProducerTypeActivity;
+import com.jcaa.usersmanagement.domain.model.Producer;
+import com.jcaa.usersmanagement.domain.valueobject.*;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProducerApplicationMapper {
+
+    public Producer fromCreateCommandToModel(final CreateProducerCommand command) {
+        return Producer.create(
+                new ProducerId(command.id()),
+                new ProducerName(command.name()),
+                ProducerEntityType.fromString(command.entityType()),
+                ProducerTypeActivity.fromString(command.typeActivity()),
+                new ProducerCountryName(command.country()),
+                new ProducerCityName(command.city()),
+                new ProducerStreetNumber(command.street()),
+                new ProducerPostalNumber(command.postal())
+        );
+    }
+
+    public ProducerId fromDeleteCommandToProducerId(final DeleteUserCommand command) {
+        return new ProducerId(command.id());
+    }
+}
